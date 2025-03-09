8b2e4e458745d7aeb8e4ccb1cca7bc503d2224e5
william vasquez <will.vasq77@gmail.com>
2025-03-08 19:44:05 -0500

    j

--- stringcleaning.js
+++ stringcleaning.js
@@ -1,28 +1,22 @@
 function strClean(arrOfStrs){
-
-   
+    function thing(arrOfStrs){
     for(let i=0;i<=arrOfStrs.length;i++){
-    let chars = {'1':'one','0':'zero','':'_'};
-
-    s = arrOfStrs[i].replace(/[0-1]/g ,m =>chars[m]);
+    let dict = ['zero','one'];
+    let x = c => dict[c];
+    console.log(arrOfStrs[i])
+        let s = arrOfStrs.replace(/[0-1]/g,x);
     s = s.replace(/[^zero one]/g ,'');
-    s = s.replace(/[\s]$/g ,'');
+    s = s.replace(/[\s]+$/g ,'');
     s = s.replace(/^[\s]/g ,'');
-
     s = s.replace(/[\s]/g ,'_');
+    return s;
 
-
-    
-
-
-
-
-
-
-return s
     }
 }
-console.log(strClean([" 1 0  1 23 ","00 0"]))
+return arrOfStrs.map(thing);
+
+}
+console.log(strClean([" 1 1  0 23  ","00 0"]))
 
 
 // DO NOT EDIT THE LINE BELOW THIS
